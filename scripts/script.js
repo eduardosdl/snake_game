45c9dc9b6ba71f902444fcf8e38fef4156fd1de5
Eduardo Silva <edu.silva.e08@gmail.com>
2023-04-16 18:10:24 -0300

    add snake moviment and eat food

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
+let snakeX = 5,
+  snakeY = 10;
+let velocityX = 0,
+  velocityY = 0;
 
 // set random number to food position
 function changeFoodPosition() {
@@ -8,11 +12,39 @@ function changeFoodPosition() {
   foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+// change direction according with key press
+function changeDirection(e) {
+  if (e.key === "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  } else if (e.key === "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  }
+}
+
 // initilize game
 function initGame() {
-  const htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  if (snakeX === foodX && snakeY === foodY) {
+    changeFoodPosition();
+  }
+
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 }
 
 changeFoodPosition();
-initGame();
+setInterval(initGame, 100);
+
+document.addEventListener("keydown", changeDirection);
